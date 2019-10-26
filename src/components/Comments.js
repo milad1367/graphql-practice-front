95f6db8ce9a6450378ba5872f5a1f4a143dc937f
milad asghari <miladasghari2016@gmail.com>
2019-10-26 15:40:09 +0330

    add base logic

---
+++ src/components/Comments.js
@@ -0,0 +1,41 @@
+import React, { useState,useEffect } from 'react'
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+import Comment from './Comment';
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useParams
+  } from "react-router-dom";
+
+function Comments() {
+    let { id } = useParams();
+    const GET_POST_COMMENTS = gql`
+        {
+            post(id: ${id}) {
+            comments {
+                name
+                content
+                id
+            }
+            }
+        }
+        `;
+    const { loading, error, data, refetch, networkStatus } = useQuery(
+        GET_POST_COMMENTS);
+    
+      if (loading) return null;
+      if (error) return `Error! ${error}`;
+      
+      const comments = data.post.comments || [];
+      return (
+        <div>
+            <Link to={'/'}>back to home</Link>
+            {comments.map(comment => <Comment key={comment.id} comment={comment} />)}
+        </div>
+      );
+}
+export default Comments
